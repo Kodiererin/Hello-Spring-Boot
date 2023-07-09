478437db5d7a4996fd3c45036add64cb5a125e14
Kodiererin <iamujjwal45@gmail.com>
2023-07-10 01:35:47 +0530

    Adding Files

---
+++ 01-Spring-boot-overview/04-actuato-security-demo/src/test/java/com/whoisUjjwal/MyCoolApp/MyCoolAppApplicationTests.java
@@ -0,0 +1,11 @@
+package com.whoisUjjwal.MyCoolApp;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
