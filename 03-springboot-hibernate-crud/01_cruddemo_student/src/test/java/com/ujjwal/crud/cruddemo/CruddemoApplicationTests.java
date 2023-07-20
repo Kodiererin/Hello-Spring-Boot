11ce676720f05e144ef81a84a62d00b3e42ec078
Kodiererin <iamujjwal45@gmail.com>
2023-07-20 05:51:29 +0530

    Adding Files

---
+++ 03-springboot-hibernate-crud/01_cruddemo_student/src/test/java/com/ujjwal/crud/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujjwal.crud.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
